d15817a497667638a66bea44c4d69f5380cbbd28
max8539 <maxyuen8539@gmail.com>
2025-01-28 04:07:46 +1100

    Fix env vars (2)

--- server/api/account/login/github.get.ts
+++ server/api/account/login/github.get.ts
@@ -1,8 +1,8 @@
 export default defineOAuthGitHubEventHandler({
   onSuccess: async (e, {user}) => {
     await setUserSession(e, {
-      clientId: process.env.NUXT_OAUTH_GITHUB_CLIENT_ID,
-      clientSecret: process.env.NUXT_OAUTH_GITHUB_CLIENT_SECRET,
+      clientId: e.context.cloudflare.env.NUXT_OAUTH_GITHUB_CLIENT_ID,
+      clientSecret: e.context.cloudflare.env.NUXT_OAUTH_GITHUB_CLIENT_SECRET,
       user: {
         userId: `github:${user.id}`
       }
--- worker-configuration.d.ts
+++ worker-configuration.d.ts
@@ -2,4 +2,9 @@
 
 interface Env {
 	CF_DB: D1Database;
+
+	// secrets
+	NUXT_OAUTH_GITHUB_CLIENT_ID: string;
+	NUXT_OAUTH_GITHUB_CLIENT_SECRET: string;
+	NUXT_SESSION_PASSWORD: string
 }
