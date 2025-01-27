caac57933c9023c303b16916af69ef8f2e3ad042
max8539 <maxyuen8539@gmail.com>
2025-01-28 04:17:05 +1100

    Fix env vars (3)

--- server/api/account/login/github.get.ts
+++ server/api/account/login/github.get.ts
@@ -1,12 +1,14 @@
 export default defineOAuthGitHubEventHandler({
   onSuccess: async (e, {user}) => {
     await setUserSession(e, {
-      clientId: e.context.cloudflare.env.NUXT_OAUTH_GITHUB_CLIENT_ID,
-      clientSecret: e.context.cloudflare.env.NUXT_OAUTH_GITHUB_CLIENT_SECRET,
       user: {
         userId: `github:${user.id}`
       }
     })
     sendRedirect(e, "/")
+  },
+  config: {
+    clientId: process.env.NUXT_OAUTH_GITHUB_CLIENT_ID,
+    clientSecret: process.env.NUXT_OAUTH_GITHUB_CLIENT_SECRET,
   }
 })
