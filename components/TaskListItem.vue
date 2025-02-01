795fd54d9551824a1cd93f8a68ca716964773ef3
cockatoo-dev <maxyuen8539@gmail.com>
2025-02-02 00:03:08 +1100

    Update task item component

--- components/TaskListItem.vue
+++ components/TaskListItem.vue
@@ -17,7 +17,7 @@
     <UCard
       v-if="isComplete"
       :ui="{ body: { padding: 'p-2 sm:p-2 lg:p-4' } }"
-      class="bg-green-600 dark:bg-green-400 group-hover:bg-green-700 dark:group-hover:bg-green-300 text-slate-100 dark:text-slate-900"
+      class="lg:h-[13.5rem] bg-green-600 dark:bg-green-400 group-hover:bg-green-700 dark:group-hover:bg-green-300 text-slate-100 dark:text-slate-900"
     >
       <div class="flex gap-2 leading-5">
         <div>
@@ -27,7 +27,7 @@
       </div>
 
       <h3
-        class="lg:h-16 line-clamp-2 overflow-ellipsis text-2xl font-bold group-hover:underline group-focus:underline"
+        class="lg:max-h-16 line-clamp-2 overflow-ellipsis text-2xl font-bold group-hover:underline group-focus:underline"
         :title="$props.title"
       >
         {{ props.title }}
@@ -39,7 +39,7 @@
     <UCard
       v-else-if="numDeps <= 0"
       :ui="{ body: { padding: 'p-2 sm:p-2 lg:p-4' } }"
-      class="bg-blue-600 dark:bg-blue-400 group-hover:bg-blue-700 dark:group-hover:bg-blue-300 text-slate-100 dark:text-slate-900"
+      class="lg:h-[13.5rem] bg-blue-600 dark:bg-blue-400 group-hover:bg-blue-700 dark:group-hover:bg-blue-300 text-slate-100 dark:text-slate-900"
     >
       <div class="flex gap-2 leading-5">
         <div>
@@ -49,7 +49,7 @@
       </div>
 
       <h3
-        class="lg:h-16 line-clamp-2 overflow-ellipsis text-2xl font-bold group-hover:underline group-focus:underline"
+        class="lg:max-h-16 line-clamp-2 overflow-ellipsis text-2xl font-bold group-hover:underline group-focus:underline"
         :title="$props.title"
       >
         {{ props.title }}
@@ -61,7 +61,7 @@
     <UCard
       v-else
       :ui="{ body: { padding: 'p-2 sm:p-2 lg:p-4' } }"
-      class="bg-red-600 dark:bg-red-400 group-hover:bg-red-700 dark:group-hover:bg-red-300 text-slate-100 dark:text-slate-900"
+      class="lg:h-[13.5rem] bg-red-600 dark:bg-red-400 group-hover:bg-red-700 dark:group-hover:bg-red-300 text-slate-100 dark:text-slate-900"
     >
       <div class="flex gap-2 leading-5">
         <div>
@@ -74,7 +74,7 @@
       </div>
 
       <h3
-        class="lg:h-16 line-clamp-2 overflow-ellipsis text-2xl font-bold group-hover:underline group-focus:underline"
+        class="lg:max-h-16 line-clamp-2 overflow-ellipsis text-2xl font-bold group-hover:underline group-focus:underline"
         :title="$props.title"
       >
         {{ props.title }}
