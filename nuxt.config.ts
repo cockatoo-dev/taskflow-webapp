ad2ff85a2dd7f0c1ed7c52fb767abd61f1c4bec5
cockatoo-dev <maxyuen8539@gmail.com>
2025-03-28 05:53:47 +1100

    Migrate wrangler.toml to wrangler.jsonc, update compatibility date

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-01-25',
+  compatibilityDate: '2025-03-27',
   devtools: { enabled: true },
 
   nitro: {
