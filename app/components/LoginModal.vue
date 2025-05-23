694a180ed3e6b80e9f76e97e8cbeb699fa80b66f
cockatoo-dev <maxyuen8539@gmail.com>
2025-05-24 05:00:47 +1000

    Update login modal text

--- app/components/LoginModal.vue
+++ app/components/LoginModal.vue
@@ -34,7 +34,7 @@
       </div>
       <div class="text-xs">
         When you log in with Github, you agree for a cookie to be saved to your web browser to store your login information for Taskflow. 
-        Taskflow does not use cookies for any other purpose. Only your Github user ID and username is collected by Taskflow, which is used to associate your Github account with the boards you create.
+        Only your Github user ID and username is collected by Taskflow, which is used to associate your Github account with the boards you create.
       </div>
     </div>
   </SmallModal>
